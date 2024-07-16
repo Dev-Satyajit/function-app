8e16e16eed06becfa292743ac66968171bc13da7
Satyajit Sinha <Satyajit.Sinha156@gmail.com>
2024-07-16 22:43:37 +0530

    Update scp.ps1

--- scp.ps1
+++ scp.ps1
@@ -1,5 +1,7 @@
-param(
+param (
+    $resourceGroup,
     $functionAppName
 )
 
+Write-Host "##vso[task.setvariable variable=TARGET_APP_RESOURCE_GROUP]$resourceGroup"
 Write-Host "##vso[task.setvariable variable=TARGET_APP_NAME]$functionAppName"
